88cda87675dbc7f24751e665b859ae71f1c6a54b
MarieSerroyen <marie.serroyen@gmail.com>
2023-01-03 16:36:59 +0100

    feat: add product image to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,8 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     @foreach($products as $product)
-                        <p>{{$product->name}}</p>
+                        <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{$product->name}}</h2>
+                        <img src="{{$product->image}}" alt="Product image">
                     @endforeach
                 </div>
             </div>
