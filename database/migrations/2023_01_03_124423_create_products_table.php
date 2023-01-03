f2178791e960be58133d539dcf3efa6db00a9692
MarieSerroyen <marie.serroyen@gmail.com>
2023-01-03 14:56:42 +0100

    feat: made products controller

--- database/migrations/2023_01_03_124423_create_products_table.php
+++ database/migrations/2023_01_03_124423_create_products_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->longText('description');
+            $table->string('price');
+            $table->string('image');
+            $table->string('quantity');
             $table->timestamps();
         });
     }
