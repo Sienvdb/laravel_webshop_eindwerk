253f3a7d2e6a48df26dcd6c64c402e4d13d678fd
MarieSerroyen <marie.serroyen@gmail.com>
2023-01-05 13:46:34 +0100

    feat: message disappears after 3 seconds

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,5 @@
 @if(session()->has('message'))
-    <div class="fixed top-0 left-1/2 transform-translate-x-1/2 bg-red-500 text-white px-48 py-3">
+    <div x-data="{show: true}" x-init="setTimeout(()=> show = false, 3000)" x-show="show" class="fixed top-0 left-1/2 transform-translate-x-1/2 bg-red-500 text-white px-48 py-3">
         <p>{{ session()->get('message') }}</p>
     </div>
 @endif
