7d7da3fda56868bb1ac3acecad5a8f88ead10753
MarieSerroyen <marie.serroyen@gmail.com>
2023-01-05 14:41:16 +0100

    feat: add content to show.blade.php for products

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,1 +1,17 @@
-test
+<x-app-layout>
+    <div class="mx-4">
+        <div class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto mt-24">
+            <h2 class="text-2xl font-bold uppercase mb-1">
+                {{$product->name}}
+            </h2>
+            <img class="w-48 mr-6 mb-6" src="{{$product->image}}" alt="Product image">
+            <p class="text-gray-600 mb-4">
+                {{$product->description}}
+            </p>
+            <p class="text-gray-600 mb-4">
+                {{$product->price}}
+            </p>
+
+        </div>
+
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/dashboard',[ProductsController::class, 'index'])->middleware(['auth
 Route::get('/dashboard/products/create',[ProductsController::class, 'create'])->middleware(['auth']);
 Route::post('/dashboard/products',[ProductsController::class, 'store'])->middleware(['auth']);
 
-Route::get('/dashboard/products/{id}/edit',[ProductsController::class, 'edit'])->middleware(['auth']);
+Route::get('/dashboard/products/{product}/edit',[ProductsController::class, 'edit'])->middleware(['auth']);
 Route::get('/dashboard/products/{product}',[ProductsController::class, 'show'])->middleware(['auth']);
 
 
