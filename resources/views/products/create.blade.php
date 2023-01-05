c32769247954c319af58e856c4d96ec02f8a34b2
MarieSerroyen <marie.serroyen@gmail.com>
2023-01-05 13:47:56 +0100

    feat: change price input to type number

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -28,7 +28,7 @@
                 <div class="mb-6">
                     <label for="price" class="inline-block text-lg mb-2">Price</label>
                     <input
-                        type="text"
+                        type="number"
                         class="border border-gray-200 rounded p-2 w-full"
                         name="price"
                     />
