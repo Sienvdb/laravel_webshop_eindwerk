f514daf67da4b82648760849748b8e1d4467f679
MarieSerroyen <marie.serroyen@gmail.com>
2023-01-05 15:20:33 +0100

    feat: add delete button to product

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -12,8 +12,14 @@
                 &euro;{{$product->price}}
             </p>
 
-            <div>
+            <div class="mt-4 p-2 flex space-x-6">
                 <a href="/dashboard/products/{{$product->id}}/edit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Edit product</a>
+
+                <form method="POST" action="/dashboard/products/{{$product->id}}">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete product</button>
+                </form>
             </div>
         </div>
 
