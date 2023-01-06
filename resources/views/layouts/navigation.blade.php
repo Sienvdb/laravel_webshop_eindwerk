343a3a98a4d1d5cdaa4ef8c926c4956389302cc2
MarieSerroyen <marie.serroyen@gmail.com>
2023-01-06 16:55:45 +0100

    feat: logo is only clickable when logged in

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,9 +5,13 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" /> 
-                    </a>
+                    @if(Auth::check())
+                        <a href="{{ route('dashboard') }}">
+                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" /> 
+                        </a>
+                    @else
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    @endif
                 </div>
                 @if(Auth::check())
                 <!-- Navigation Links -->
