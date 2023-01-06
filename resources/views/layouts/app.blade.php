39dfb34bcc1a5a7cd9b824610d8220ae7523cd7b
MarieSerroyen <marie.serroyen@gmail.com>
2023-01-06 15:16:27 +0100

    style: search bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,5 +33,7 @@
                 {{ $slot }}
             </main>
         </div>
+
     </body>
+
 </html>
--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -1,8 +1,5 @@
 <form action="/dashboard">
-    <div class="relative border-2 border-gray-100 m-4 rounded-lg">
-      <div class="absolute top-4 left-3">
-        <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
-      </div>
+    <div class="relative border-2 border-gray-100 my-4 mx-20 rounded-lg">
       <input type="text" name="search" class="h-14 w-full pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
         placeholder="Search Valentijnshop" />
       <div class="absolute top-2 right-2">
