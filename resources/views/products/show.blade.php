46941ef3efa9fa4f10e80b17f1f4a15a039c806c
MarieSerroyen <marie.serroyen@gmail.com>
2023-01-06 16:52:38 +0100

    style: product card

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,16 +1,17 @@
 <x-app-layout>
     <div class="mx-4">
         <div class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto mt-24">
-            <h2 class="text-2xl font-bold uppercase mb-1">
+            <h2 class="text-2xl font-bold uppercase mb-6 text-red-600 flex justify-center">
                 {{$product->name}}
             </h2>
-            <img class="w-48 mr-6 mb-6" src="{{$product->image}}" alt="Product image">
-            <p class="text-gray-600 mb-4">
-                {{$product->description}}
-            </p>
-            <p class="text-gray-600 mb-4">
+            <img class="w-48 mr-6 mb-6 flex items-center justify-center" src="{{$product->image}}" alt="Product image">
+            <p class="text-black mb-4">
                 &euro;{{$product->price}}
             </p>
+            <p class="text-black mb-4">
+                {{$product->description}}
+            </p>
+
 
             @if(Auth::check())
                 <div class="mt-4 p-2 flex space-x-6">
@@ -25,7 +26,7 @@
             @endif
 
             @if(!Auth::check())
-            <div class="mt-4 p-2 flex space-x-6">
+            <div class="mt-4 flex space-x-6">
 
                 <form method="POST" action="#">
                     @csrf
