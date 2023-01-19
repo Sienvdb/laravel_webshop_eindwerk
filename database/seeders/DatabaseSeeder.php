c94ae977c4b54b0de22aa98f0d9c3ff2bed79569
Sienvdb <sienvdbergh@gmail.com>
2023-01-19 12:27:25 +0100

    feat: add order factory (not nessecary)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Product;
+use App\Models\Order;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          Product::factory(6)->create();
+         Order::factory(6)->create();
 
 
     }
