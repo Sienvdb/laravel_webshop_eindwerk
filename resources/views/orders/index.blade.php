d076ae4bd30da1525c3954778e137c1391f84cf1
Sienvdb <sienvdbergh@gmail.com>
2023-01-22 19:55:18 +0100

    feat: add price to orders

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -2,21 +2,24 @@
     <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <a class="btn btn-primary" href="{{route('mollie.create.payment')}}">Pay</a>
+            <p>Total cost = {{$total_cost}}</p>
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-20">
-                <a class="btn btn-primary" href="{{route('mollie.create.payment')}}">Pay</a>
-
                 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+
                     @foreach($products as $product)
                     @foreach($orders as $order)
-                    @foreach($my_price as $key => $value)
                         @if($order->product_id == $product->id)
+                        @php
+                            $price = $product->price * $order->amount;
+                        @endphp
                         <div class="flex">
                             <!--<img class="w-48 mr-6 md:block" src="{{$product->image ? asset('storage/' . $product->image) : asset($product->image)}}" alt="Product image">-->
                             <img class="w-48 mr-6 md:block" src="{{$product->image}}" alt="Product image">
                             <div class="text-2xl">
                                 <a href="/dashboard/products/{{$product->id}}" class="font-semibold text-xl text-red-600 leading-tight">{{$product->name}}</a>
                                 <p>Amount: {{$order->amount}}</p>
-                                <p>Price: {{$my_price[$key]}} </p>
+                                <p>Price: {{$price}} </p>
                                 <form method="POST" action="/orders/{{$order->id}}">
                                     @csrf
                                     @method('DELETE')
@@ -25,7 +28,6 @@
                             </div> 
                         </div>      
                         @endif  
-                        @endforeach                
                         @endforeach
                     @endforeach
                 </div>
