eafda1d68f2f0c9e0fd9a269bce59406c6659edc
Sienvdb <sienvdbergh@gmail.com>
2023-01-22 19:55:41 +0100

    feat: add status to migrations

--- database/migrations/2023_01_04_102214_create_orders_table.php
+++ database/migrations/2023_01_04_102214_create_orders_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->integer('product_id');
             $table->longText('description');
             $table->integer('amount')->default(1);
+            $table->string('status')->default('pending');
             $table->timestamps();
         });
     }
