a2842b0996961125c20b55772d8b74dc39a2ce0d
Sienvdb <sienvdbergh@gmail.com>
2023-01-23 17:56:50 +0100

    feat: add styling search form

--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -1,9 +1,9 @@
 <form action="/dashboard">
-    <div class="relative border-2 border-pink-100 my-4 mx-20 rounded-lg">
-      <input type="text" name="search" class="h-14 w-full pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
+    <div class="relative border-2 border-rose-700 my-4 mx-20 rounded-lg bg-rose-100 text-rose-700">
+      <input type="text" name="search" class=" w-full text-rose-700 rounded-lg z-0 focus:shadow focus:outline-none"
         placeholder="Search Valentijnshop" />
       <div class="absolute top-2 right-2">
-        <button type="submit" class="h-10 w-20 text-white rounded-lg bg-red-500 hover:bg-red-600">
+        <button type="submit" class="p-1.5 w-20 bg-rose-700 text-white border border-rose-700 rounded-lg bg-red-500 hover:bg-red-600">
           Search
         </button>
       </div>
