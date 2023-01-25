cbd141e637a3612683fa67aa0ab71320ba45a436
Sienvdb <sienvdbergh@gmail.com>
2023-01-25 11:29:28 +0100

    fix: styling webshop

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,15 +15,13 @@
 
                     <div class="lg:grid lg:grid-cols-2 gap-6 space-y-6 md:space-y-0 mt-5">
                         @foreach($products as $product)
-                            <a href="/products/{{$product->id}}">
-                                <div class="flex bg-rose-300 rounded p-5">
-                                    <a href="/products/{{$product->id}}"><img class="w-48 mr-6 md:block rounded" src="{{$product->image ? asset('storage/' . $product->image) : '/images/default.jpg'}}" alt="Product image"></a>
-                                    <div class="text-2xl grid grid-column-4 grid-rows-3">
-                                        <a href="/products/{{$product->id}}" class="col-start-1 col-end-4 text-3xl font-serif text-rose-700 leading-tight">{{$product->name}}</a>
-                                        <p class="col-start-1 col-end-2 row-span1 text-base text-rose-100 mt-2 bg-rose-700 rounded block break-normal p-2 pb-0">&euro;{{$product->price}}</p>
-                                    </div> 
-                                </div>
-                            </a>
+                            <div class="flex bg-rose-300 rounded p-5">
+                                <a href="/products/{{$product->id}}"><img class="w-48 mr-6 md:block rounded" src="{{$product->image ? asset('storage/' . $product->image) : '/images/default.jpg'}}" alt="Product image"></a>
+                                <div class="text-2xl grid grid-column-4 grid-rows-3">
+                                    <a href="/products/{{$product->id}}" class="col-start-1 col-end-4 text-3xl font-serif text-rose-700 leading-tight">{{$product->name}}</a>
+                                    <p class="col-start-1 col-end-2 row-span1 text-base text-rose-100 mt-2 bg-rose-700 rounded block break-normal p-2 pb-0">&euro;{{$product->price}}</p>
+                                </div> 
+                            </div>
                         @endforeach
                 </div>
             </div>
